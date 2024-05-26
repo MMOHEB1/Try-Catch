b96a7020966f2b4118056f216de4aa864324ed17
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-05-26 21:27:38 +0100

    commit 2

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-game_is_on = True
-while game_is_on:
+def generate_phoenetic():
+    word = input("Enter a word: ").upper()
     try:
-        word = input("Enter a word: ").upper()
         output_list = [phonetic_dict[letter] for letter in word]
-        print(output_list)
     except KeyError:
         print("Sorry only letter in the alphabet please.")
+    else:
+        print(output_list)
 
